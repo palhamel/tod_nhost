4326e8e4951ce21dac23e0386a15f0e624a5e1d6
Pål Hamel <pal.hamel@gmail.com>
2020-09-16 11:12:34 +0200

    add input form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 
+// Get data:
 const GET_TODOS = gql`
   query {
     todos {
@@ -14,9 +15,20 @@ const GET_TODOS = gql`
 `;
 // console.log(GET_TODOS);
 
+// Add data:
+const INSERT_TODO = gql`
+  mutation($todo: todos_insert_input!) {
+    insert_todos(objects: [$todo]) {
+      affected_rows
+    }
+  }
+`;
+
 function App() {
   const { data, loading } = useQuery(GET_TODOS);
   // console.log('dataprop:', data);
+  const [insertTodo] = useMutation(INSERT_TODO);
+  const [todoName, setTodoName] = useState("");
 
   if (loading) {
     return <div>Loading...</div>;
@@ -33,8 +45,38 @@ function App() {
           })}
         </ul>
       )}
+
+      <form 
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          try {
+            await insertTodo({
+              variables: {
+                todo: {
+                  name: todoName,
+                },
+              },
+            });
+          } catch (error) {
+            alert("Error creating todo");
+            console.log(error);
+          }
+          alert("todo created");
+          setTodoName("");
+        }}
+      >
+        <input 
+          type="text" 
+          placeholder="todo" 
+          value={todoName} 
+          onChange={ (e) => setTodoName(e.target.value)}
+        />
+
+      </form>
+
     </div>
   );
-}
+};
 
 export default App;
