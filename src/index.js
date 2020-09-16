9d115eddaff234e36a040385276b0026c38571a4
Pål Hamel <pal.hamel@gmail.com>
2020-09-16 10:15:33 +0200

    add dep, config and nhost url

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { NhostApolloProvider } from 'react-nhost'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NhostApolloProvider gqlEndpoint="https://hasura-wbfqe0lb.nhost.app/v1/graphql">
+      <App />
+    </NhostApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
