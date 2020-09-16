ab3b3237de24fff5ab40c760a9af004dc8dd0ac3
Pål Hamel <pal.hamel@gmail.com>
2020-09-16 10:27:03 +0200

    add query for graphql

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag'
+
+const GET_TODOS = gql`
+  query {
+    todos {
+      id
+      created_at
+      name
+      completed
+    }
+  }
+`; 
+
 
 function App() {
+
+  const { data, loading } = useQuery(GET_TODOS);
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { NhostApolloProvider } from 'react-nhost'
 import App from './App';
-
+// import './index.css';
+// const URL_Nhost = "https://hasura-wbfqe0lb.nhost.app/v1/graphql"
 
 ReactDOM.render(
   <React.StrictMode>
