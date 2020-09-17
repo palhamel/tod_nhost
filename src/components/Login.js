dff853f121f69b16059a9fe80e3946f4e21bdbe9
Pål Hamel <pal.hamel@gmail.com>
2020-09-17 10:12:00 +0200

    add handleSubmit

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,3 +2,31 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { auth } from "util/nhost";
 
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  // handleSubmit as async func
+  async function handleSubmit(e) {
+    // stop default refreshing
+    e.preventDefault();
+    // login
+    try {
+      await auth.login(email, password);
+    } catch (error) {
+      alert("error logging in");
+      console.error(error)
+      return;
+    }
+    // if no error redirect back
+    history.push("/");
+  }
+
+
+
+  // form
+  return (
+
+  )
+}
