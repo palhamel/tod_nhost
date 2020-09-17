661790d5a280ffa7ad8566e357a70e4ecc38c324
Pål Hamel <pal.hamel@gmail.com>
2020-09-17 10:36:05 +0200

    add form and finish login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,6 @@ export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history = useHistory();
-
   // handleSubmit as async func
   async function handleSubmit(e) {
     // stop default refreshing
@@ -15,18 +14,32 @@ export default function Login(props) {
     try {
       await auth.login(email, password);
     } catch (error) {
-      alert("error logging in");
-      console.error(error)
+      alert("Houston - we have an error logging in");
+      console.error(error);
       return;
     }
-    // if no error redirect back
+    // if no error redirect back - or "./"?
     history.push("/");
   }
 
-
-
-  // form
+  // build form;
   return (
-
-  )
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
